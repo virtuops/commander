476364b7a977bdf6bc2556996ef5d4671760bbfc
virtuops <support@virtuops.com>
2018-03-21 11:48:00 -0400

    CSS Updates

--- html/docs/intro.php
+++ html/docs/intro.php
@@ -2,21 +2,21 @@
 
 ?>
 <div style="margin: 20px;">
-<p style="text-align: center;"><img src="libs/css/images/nochero_intro_800x214.png" alt="nochero_intro"/></p>
+<p style="text-align: center;"><img src="html/images/1.1.png" alt="virtuops_commander_logo"/></p>
 <p>&nbsp;</p>
-<h1 style="text-align: center;"><span style="color: #333333;">Welcome to <span style="color: #000000; font-weight: bold;">VirtuOps&trade;</span></span><br><span style="color: red; font-weight: bold;"></span>
+<h1 style="text-align: center;"><span style="color: #lightslategray;">Welcome to <span style="color: #000000; font-weight: bold;">VirtuOps&trade; Commander</span></span><br><span style="color: red; font-weight: bold;"></span>
 		</h1>
 <p>&nbsp;</p>
 <blockquote>
-<p><span style="color: #333333;"><span style="color: #000000; font-weight: bold;">VirtuOps&trade; Commander</span> is the platform of choice for visualization and operator interaction with your open source tools. &nbsp;If you have a large, expensive framework tool and are afraid to walk away from it because you rely heavily on your charts, graphs, dashboards, event lists and right click(context) tools, fear no more! &nbsp;We can give you the same (or better) capabilities in a fraction of the time.  Use us with Zabbix, Nagios, Cacti, OpenNMS, MRTG or any other free(ish) tool and get the same visualizations and operator interaction that you enjoy in enterprise systems like Netcool, HP, CA, SolarWinds.  &nbsp;<span style="color: #000000; font-weight: bold;">VirtuOps&trade; Commander</span> serves as the focal point for users and third party tools to work together to remediate a situation as quickly as possible.</p>
+<p><span style="color: #lightslategray;"><span style="color: #darkslategray; font-weight: bold;">VirtuOps&trade; Commander</span> is the platform of choice for visualization and operator interaction with your open source tools. &nbsp;If you have a large, expensive framework tool and are afraid to walk away from it because you rely heavily on your charts, graphs, dashboards, event lists and right click(context) tools, fear no more! &nbsp;We can give you the same (or better) capabilities in a fraction of the time.  Use us with Zabbix, Nagios, Cacti, OpenNMS, MRTG or any other free(ish) tool and get the same visualizations and operator interaction that you enjoy in enterprise systems like Netcool, HP, CA, SolarWinds.  &nbsp;<span style="color: #darkslategray; font-weight: bold;">VirtuOps&trade; Commander</span> serves as the focal point for users and third party tools to work together to remediate a situation as quickly as possible.</p>
 <p>&nbsp;</p>
 <p>To get started, please choose an item below:</p>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
-<button class="introbtn" type="button" onclick="window.open('http://www.virtuops.com/nochero-commander-how-to-guides')">How To Guides</button>
+<button class="introbtn" type="button" onclick="window.open('https://www.virtuops.com/nochero-commander-how-to-guides')">How To Guides</button>
 <p>&nbsp;</p>
-<button class="introbtn" type="button" onclick="window.open('http://www.virtuops.com/nochero-documentation')">Documentation</button>
+<button class="introbtn" type="button" onclick="window.open('https://www.virtuops.com/nochero-documentation')">Documentation</button>
 <p>&nbsp;</p>
-<button class="introbtn" type="button" onclick="window.open('http://www.virtuops.com')">VirtuOps Website</button>
+<button class="introbtn" type="button" onclick="window.open('https://www.virtuops.com')">VirtuOps Website</button>
 </blockquote>
 </div>
